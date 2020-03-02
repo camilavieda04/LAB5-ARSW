5448e76c124d7b717d13ec062d26a51d0747bc49
unknown <sarah.vieda@labinfo.is.escuelaing.edu.co>
2020-03-02 11:58:07 -0500

    avance

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,3 +1,20 @@
-(function () {
-  // code
+app= (function (){
+    var lista = function (variable) {
+        if(variable != null){
+            alert(variable.length)
+            var diccionario = variable.map(function(bp){
+                alert(bp.name)
+                alert(bp.points.length)
+                return {key:bp.name, value:bp.points.length}
+            })
+            alert(diccionario)
+        }
+    };
+    return {
+            prueba: function () {
+                author = document.getElementById("fname").value;
+                alert(author)
+                apimock.getBlueprintsByAuthor(author,lista);
+            }
+        };
 })();
