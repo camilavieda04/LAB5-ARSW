304fff07f642f2bf63c3c8fcbba797f996065f49
unknown <sarah.vieda@labinfo.is.escuelaing.edu.co>
2020-03-02 16:38:10 -0500

    fin

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,19 +1,27 @@
 app= (function (){
     var lista = function (variable) {
         if(variable != null){
-            alert(variable.length)
+            //alert(variable.length)
             var diccionario = variable.map(function(bp){
-                alert(bp.name)
-                alert(bp.points.length)
+                //alert(bp.name)
+                //alert(bp.points.length)
                 return {key:bp.name, value:bp.points.length}
             })
-            alert(diccionario)
+            $("#tabla tbody").empty();
+            diccionario.map(function(bp){
+                var temp = '<tr><td id="nombreActor">'+bp.key+'</td><td id="puntos">'+bp.value+'</td></tr>';
+                $("#tabla tbody").append(temp);
+            })
+            var valorTotal = diccionario.reduce(function(total, valor){
+                return total.value + valor.value;
+            })
+            alert(valorTotal)
         }
     };
     return {
             prueba: function () {
                 author = document.getElementById("fname").value;
-                alert(author)
+                //alert(author)
                 apimock.getBlueprintsByAuthor(author,lista);
             }
         };
